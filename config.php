b93db37bfc1846c31bd51331f10b55588ce6b6d3
Mikhail Guseletov <mvguseletov@bars.group>
2025-04-02 14:54:55 +0700

    Удаление лишнего

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-const TOKEN = '123';
-const CONFIRMATION_TOKEN = 'cb19c86e'; //'2f7d0190'; //'ac5610b4';//'5d119205';
-//const USER_ID = 5978179;
-const VK_API_VERSION = '5.67';
-const VK_API_ENDPOINT = 'https://api.vk.com/method/';
-?>
